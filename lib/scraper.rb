0f930748c70a125f28270083de7671319d6400de
Christian Perez <perezchristian1012@gmail.com>
2019-09-22 00:59:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN").text
+html = open("https://web.archive.org/web/20160227204808/http://flatironschool.com/team")
+doc = Nokogiri::HTML(html)
+
+
+instructors = doc.css("#instructors .team-holder .person-box")
+
+instructors.each do |instructor|
+puts "Flatiron School <3 " + instructor.css("h2").text
+end
